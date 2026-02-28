44fe84dd10a9a8c935f96127ecc339cec5785d47
Alexis Kiourtzidis <kiourtzidisalexis@gmail.com>
2026-02-28 14:06:11 +0200

    Scientific: Add history frame

--- ui/scientific_ui.py
+++ ui/scientific_ui.py
@@ -4,9 +4,20 @@ class ScientificUI(ctk.CTkFrame):
 
     def __init__(self, parent):
 
-        super().__init__(parent, fg_color='#2E2E2E')
+        super().__init__(parent, fg_color='#1F1F1F')
+
+        self.max_history_chars = 15
+
         self.width = 720
         self.height = 615
 
-        label = ctk.CTkLabel(self, text='Coming soon...', font=('Jetbrains Mono', 24))
-        label.pack(expand=True)
+        self.grid_rowconfigure(0, weight=5)
+        self.grid_rowconfigure(1, weight=0)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.history_frame = ctk.CTkFrame(self, fg_color='#3C3C3C')
+        self.history_frame.grid(row=0, column=0, sticky='nsew', padx=10, pady=(8, 4))
+        self.history_frame.grid_rowconfigure(0, weight=0)
+        self.history_frame.grid_rowconfigure(1, weight=1)
+        self.history_frame.grid_columnconfigure(0, weight=1)
