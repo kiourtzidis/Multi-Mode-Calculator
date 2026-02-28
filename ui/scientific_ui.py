88df01df7b023bf423fcbc9d6b76bf136ece90bf
Alexis Kiourtzidis <kiourtzidisalexis@gmail.com>
2026-02-28 14:09:25 +0200

    Scientific: Add Clear History button

--- ui/scientific_ui.py
+++ ui/scientific_ui.py
@@ -21,3 +21,14 @@ class ScientificUI(ctk.CTkFrame):
         self.history_frame.grid_rowconfigure(0, weight=0)
         self.history_frame.grid_rowconfigure(1, weight=1)
         self.history_frame.grid_columnconfigure(0, weight=1)
+
+        self.clear_history_button = ctk.CTkButton(
+            self.history_frame,
+            text='Clear History',
+            height=28,
+            fg_color='#2A2A2A',
+            hover_color='#323232',
+            font=('Jetbrains Mono', 14)
+        )
+        self.clear_history_button.grid(row=0, column=0, sticky='nsew', padx=4, pady=(4, 2))
+        self.clear_history_button.configure(cursor='hand2')
