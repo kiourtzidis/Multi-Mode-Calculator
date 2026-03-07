22276c8d3543fedbff266ea4dd13f7ce557e818f
Alexis Kiourtzidis <kiourtzidisalexis@gmail.com>
2026-03-07 22:27:41 +0200

    Scientific: Add functionality to trigonometry buttons

--- logic/calculator_logic.py
+++ logic/calculator_logic.py
@@ -8,8 +8,17 @@ class CalculatorLogic:
         self.eval_expression = ''
         self.calculated = False
         self.angle_mode = 'DEG'
-        self.eval_functions = {**math.__dict__, 'cbrt': self.cbrt}
         self.operators = ('+', '-', '×', '÷', 'mod', 'div')
+        self.eval_functions = {
+            **math.__dict__,
+            'cbrt': self.cbrt,
+            'sin': self.sin,
+            'cos': self.cos,
+            'tan': self.tan,
+            'arcsin': self.arcsin,
+            'arccos': self.arccos,
+            'arctan': self.arctan
+            }
         self.convert_buttons = {
             '×': {
                 'append': '×',
@@ -45,15 +54,15 @@ class CalculatorLogic:
             },
             'arcsin': {
                 'append': 'arcsin(',
-                'calculate': 'asin('
+                'calculate': 'arcsin('
             },
             'arccos': {
                 'append': 'arccos(',
-                'calculate': 'acos('
+                'calculate': 'arccos('
             },
             'arctan': {
                 'append': 'arctan(',
-                'calculate': 'atan('
+                'calculate': 'arctan('
             },
             'log': {
                 'append': 'log(',
@@ -207,3 +216,42 @@ class CalculatorLogic:
     
     def cbrt(self, x):
         return x ** (1/3) 
+    
+
+    def sin(self, x):
+        if self.angle_mode == 'DEG':
+            x = math.radians(x)
+        return round(math.sin(x), 12)
+
+
+    def cos(self, x):
+        if self.angle_mode == 'DEG':
+            x = math.radians(x)
+        return round(math.cos(x), 12)
+    
+
+    def tan(self, x):
+        if self.angle_mode == 'DEG':
+            x = math.radians(x)
+        return round(math.tan(x), 12)
+    
+
+    def arcsin(self, x):
+        result = math.asin(x)
+        if self.angle_mode == 'DEG':
+            result = math.degrees(result)
+        return round(result, 12)    
+
+
+    def arccos(self, x):
+        result = math.acos(x)
+        if self.angle_mode == 'DEG':
+            result = math.degrees(result)
+        return round(result, 12)    
+
+
+    def arctan(self, x):
+        result = math.atan(x)
+        if self.angle_mode == 'DEG':
+            result = math.degrees(result)
+        return round(result, 12)
