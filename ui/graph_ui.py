75a7bb35590a4bc9f546866d2624bfa2bbfa26d4
Alexis Kiourtzidis <kiourtzidisalexis@gmail.com>
2026-03-29 23:44:27 +0300

    Graph: Create the frames (Canvas, Controls, Buttons)

--- ui/graph_ui.py
+++ ui/graph_ui.py
@@ -4,9 +4,32 @@ class GraphUI(ctk.CTkFrame):
 
     def __init__(self, parent):
         
-        super().__init__(parent, fg_color='#2E2E2E')
-        self.width = 460
-        self.height = 715
+        super().__init__(parent, fg_color='#1F1F1F')
 
-        label = ctk.CTkLabel(self, text='Coming soon...', font=('Jetbrains Mono', 24))
-        label.pack(expand=True)
+        self.width = 500
+        self.height = 620
+
+        self.grid_rowconfigure(0, weight=0)
+        self.grid_rowconfigure(1, weight=0)
+        self.grid_rowconfigure(2, weight=0)
+        self.grid_columnconfigure(0, weight=1)
+
+        self._build_canvas()
+        self._build_controls()
+        self._build_buttons()
+
+
+    def _build_canvas(self):
+
+        self.canvas_frame = ctk.CTkFrame(self, fg_color='#2E2E2E', width=self.width, height=300)
+        self.canvas_frame.grid(row=0, column=0, sticky='nsew', padx=10, pady=(8, 4))
+
+    
+    def _build_controls(self):
+        self.controls_frame = ctk.CTkFrame(self, fg_color='#2E2E2E', height=70)
+        self.controls_frame.grid(row=1, column=0, sticky='nsew', padx=10, pady=(8, 4))
+
+
+    def _build_buttons(self):
+        self.buttons_frame = ctk.CTkFrame(self, fg_color='#1F1F1F', height=165)
+        self.buttons_frame.grid(row=2, column=0, sticky='nsew', padx=10, pady=10)
