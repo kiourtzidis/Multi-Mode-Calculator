f07c8939f3754a140a426c337a034f64063f729e
Alexis Kiourtzidis <kiourtzidisalexis@gmail.com>
2026-04-02 14:17:52 +0300

    Graph: Replace eval() with Sympy functions

--- logic/graph_logic.py
+++ logic/graph_logic.py
@@ -1,4 +1,5 @@
-import numpy as np
+import sympy as sp
+from sympy import symbols, sympify, lambdify
 from logic.calculator_logic import CalculatorLogic
 
 class GraphLogic(CalculatorLogic):
@@ -7,36 +8,47 @@ class GraphLogic(CalculatorLogic):
             
             super().__init__()
             
-            self.numpy_functions = {
-            'sin': np.sin,
-            'cos': np.cos,
-            'tan': np.tan,
-            'sec': lambda x: 1/np.cos(x),
-            'csc': lambda x: 1/np.sin(x),
-            'cot': lambda x: 1/np.tan(x),
-            'arcsin': np.arcsin,
-            'arccos': np.arccos,
-            'arctan': np.arctan,
-            'arcsec': lambda x: np.arccos(1/x),
-            'arccsc': lambda x: np.arcsin(1/x),
-            'arccot': lambda x: np.arctan(1/x),
-            'x²': lambda x: x**2,
-            'x³': lambda x: x**3,
-            'log': np.log10,
-            'log₂': np.log2,
-            'ln': np.log,
-            'sqrt': np.sqrt,
-            'cbrt': np.cbrt,
-            'abs': np.abs,
-            'exp': np.exp,
-            'pi': np.pi,
-            'e': np.e
+            self.sympy_functions = {
+               'x': symbols('x'),
+
+               'sin': sp.sin,
+               'cos': sp.cos,
+               'tan': sp.tan,
+
+               'sec': lambda x: 1/sp.cos(x),
+               'csc': lambda x: 1/sp.sin(x),
+               'cot': lambda x: 1/sp.tan(x),
+
+               'arcsin': sp.asin,
+               'arccos': sp.acos,
+               'arctan': sp.atan,
+
+               'log': sp.log,
+               'log10': lambda x: sp.log(x, 10),
+               'log2': lambda x: sp.log(x, 2),
+
+               'sqrt': sp.sqrt,
+               'abs': sp.Abs,
+               'exp': sp.exp,
+
+               'pi': sp.pi,
+               'e': sp.E
         }
             
         def evaluate_graph(self, x):
 
-             try:
-                return eval(self.eval_expression, {'__builtins__': {}}, {**self.numpy_functions, 'x': x})
-             
-             except:
-                return None
+            try:
+               print("EXPR:", self.eval_expression)
+               x_symbol = self.sympy_functions['x']
+
+               graph_expression = sympify(self.eval_expression, locals=self.sympy_functions)
+               print("SYMPY EXPR:", graph_expression)
+               f = lambdify(x_symbol, graph_expression, 'numpy')
+
+               y = f(x)
+
+               return y
+
+            except Exception as e:
+               print("Error evaluating graph:", e)
+               return None
