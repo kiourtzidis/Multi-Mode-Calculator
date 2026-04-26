1f015a84aaca6de7d47b10b6981a31a379f6fab1
kiourtzidis <kiourtzidisalexis@gmail.com>
2026-04-26 17:32:42 +0300

    Fix formatting

--- logic/graph_logic.py
+++ logic/graph_logic.py
@@ -3,16 +3,19 @@ from logic.calculator_logic import CalculatorLogic
 
 class GraphLogic(CalculatorLogic):
 
-        def evaluate_graph(self, x):
-         try:
+    def evaluate_graph(self, x):
+
+        try:
             parser = Parser(self.tokens)
+            print(self.tokens)
             ast = parser.parse()
+            print(f'ast: {ast}')
             scope = {
                   'x': x,
                   **self.function_library
             }
             return ast.evaluate(scope)
 
-         except Exception as e:
+        except Exception as e:
             print(f'Error: {e}')
             return None
