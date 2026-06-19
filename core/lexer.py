059e8394f21dc215791418ec108093eac0a520d1
kiourtzidis <kiourtzidisalexis@gmail.com>
2026-06-19 16:20:16 +0300

    Add ˣ to the allowed expression characters

--- core/lexer.py
+++ core/lexer.py
@@ -5,7 +5,7 @@ class Lexer:
 
     def __init__(self):
 
-        self.expression_pattern = re.compile(r'^[0-9+\-*/^().,%‰|!a-zA-Z×÷π₂⁻¹²³ʸ√∛]*$')
+        self.expression_pattern = re.compile(r'^[0-9+\-*/^().,%‰|!a-zA-Z×÷π₂⁻¹²³ˣʸ√∛]*$')
         self.token_map = {
             '+': (TokenType.INFIX_OPERATOR, '+', '+', '+'),
             '-': (TokenType.INFIX_OPERATOR, '-', '-', '-'),
