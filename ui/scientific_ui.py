9e5b8a5086437eba551aab6d89a68bd2f065ff00
kiourtzidis <kiourtzidisalexis@gmail.com>
2026-06-30 21:54:31 +0300

    Remove border of angle switch button

--- ui/scientific_ui.py
+++ ui/scientific_ui.py
@@ -24,8 +24,6 @@ class ScientificUI(CalculatorUI):
             height=34,
             fg_color='#262626',
             hover_color='#3A3A3A',
-            border_width=2,
-            border_color='#565B5E',
             font=('Jetbrains Mono', 26),
             command=self.toggle_angle
         )
